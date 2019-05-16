e2faff2fd5ed9271501d4b009a82acaab1f675bb
Philippe Gadiou <philippe.gadiou@gmail.com>
2019-05-16 17:48:56 +0200

    search example AB

--- forest/customer_stats.js
+++ forest/customer_stats.js
@@ -4,13 +4,13 @@ const models = require('../models');
 Liana.collection('customer_stats', {
   isSearchable: true,
   fields: [{
-      field: 'amount',
-      type: 'String'
-    }, {
-      field: 'orders',
-      type: 'String',
-    }, {
       field: 'email',
       type: 'String',
+    }, {
+      field: 'orders',
+      type: 'Number',
+    }, {
+      field: 'amount',
+      type: 'Number',
     }]
 });
--- routes/customer_stats.js
+++ routes/customer_stats.js
@@ -9,101 +9,68 @@ router.get('/customer_stats', Liana.ensureAuthenticated, (req, res, next) => {
   const limit = parseInt(req.query.page.size) || 20;
   const offset = (parseInt(req.query.page.number) - 1) * limit;
   const queryType = models.sequelize.QueryTypes.SELECT;
-  const searchTerm = '%' + req.query.search + '%'
+  let conditionSearch = '';
 
-  const dataQuery = `
+  if (req.query.search) {
+    conditionSearch = `customers.email LIKE '%${req.query.search}%'`;
+  }
+
+  const queryData = `
     SELECT customers.id,
-    customers.email,
-    count(orders.*) AS orders,
-    sum(products.price) AS amount,
-    customers.created_at,
-    customers.updated_at
+      customers.email,
+      count(orders.*) AS orders,
+      sum(products.price) AS amount,
+      customers.created_at,
+      customers.updated_at
     FROM customers
     JOIN orders ON customers.id = orders.customer_id
     JOIN products ON orders.product_id = products.id
+    ${conditionSearch ? `WHERE ${conditionSearch}` : ''}
     GROUP BY customers.id
     ORDER BY customers.id
-    limit ${limit}
-    offset ${offset}
+    LIMIT ${limit}
+    OFFSET ${offset}
   `;
 
-    const countQuery = `
-      SELECT COUNT(*)
-      FROM customers
-      WHERE EXISTS (SELECT *
-        FROM orders
-        WHERE orders.customer_id = customers.id);
-    `;
+  console.log(queryData)
 
-  const searchQuery = `
-    SELECT customers.id,
-    customers.email,
-    count(orders.*) AS orders,
-    sum(products.price) AS amount,
-    customers.created_at,
-    customers.updated_at
+  const queryCount = `
+    SELECT COUNT(*)
     FROM customers
-    JOIN orders ON customers.id = orders.customer_id
-    JOIN products ON orders.product_id = products.id
-    WHERE customers.email
-    LIKE '${searchTerm}'
-    GROUP BY customers.id
-    limit ${limit}
-    offset ${offset}
+    WHERE
+      EXISTS (
+        SELECT *
+        FROM orders
+        WHERE orders.customer_id = customers.id
+      )
+      ${conditionSearch ? `AND ${conditionSearch}` : ''}
   `;
 
-    if (req.query.search) {
+  console.log(queryCount)
 
-      return P
-          .all([
-            models.sequelize.query(searchQuery, { type: queryType })
-          ])
-          .then((customer_stats_data) => {
-            const customerStatsSerializer = new JSONAPISerializer('customer_stats', {
-              attributes: ["email", "orders", "amount"]
-            });
-            const customer_stats = customerStatsSerializer.serialize(customer_stats_data[0]);
-            res.send(customer_stats);
-          })
-          .catch((err) => next(err));
-    }
-
-    else {
-      return P
-          .all([
-            models.sequelize.query(countQuery, { type: queryType }),
-            models.sequelize.query(dataQuery, { type: queryType })
-          ])
-          .spread((count_data, customer_stats_data) => {
-            return customer_stats_info = {data: customer_stats_data, count: count_data[0].count}
-          })
-          .then((customer_stats_info) => {
-            const customerStatsSerializer = new JSONAPISerializer('customer_stats', {
-              attributes: ["email", "orders", "amount"]
-            });
-            const customer_stats = customerStatsSerializer.serialize(customer_stats_info.data);
-            const count = customer_stats_info.count
-            res.send({...customer_stats, meta:{ count: count}});
-          })
-          .catch((err) => next(err));
-    }
-
+  return P
+    .all([
+      models.sequelize.query(queryData, { type: queryType }),
+      models.sequelize.query(queryCount, { type: queryType }),
+    ])
+    .spread((customerStatsList, customerStatsCount) => {
+      const customerStatsSerializer = new JSONAPISerializer('customer_stats', {
+        attributes: ['email', 'orders', 'amount']
+      });
+      const customerStats = customerStatsSerializer.serialize(customerStatsList);
+      const count = customerStatsCount[0].count
+      res.send({ ...customerStats, meta:{ count: count }});
+    })
+    .catch((err) => next(err));
 });
-
-// router.get('/customer_stats/count', Liana.ensureAuthenticated, (req, res, next) => {
-//   res.send({count: 50})
-// });
 
 router.get('/customer_stats/:id', Liana.ensureAuthenticated, function (req, res) {
   res.status(400).send('Implement the GET in /forest/customer_stats/:id.');
 });
-
 router.post('/customer_stats', Liana.ensureAuthenticated, function (req, res) {
   res.status(400).send('Implement the POST in /forest/customer_stats');
 });
-
 router.put('/customer_stats/:id', Liana.ensureAuthenticated, function (req, res) {
   res.status(400).send('Implement the PUT in /forest/customer_stats/:id.');
 });
-
 module.exports = router;
