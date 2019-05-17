711ddf3fc994add54bdfd2cdee0eb1046693ffd6
Philippe Gadiou <philippe.gadiou@gmail.com>
2019-05-17 14:35:24 +0200

    cors fixed v3

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const cors = require('express-cors');
+const cors = require('cors');
 const jwt = require('express-jwt');
 
 const app = express();
@@ -19,8 +19,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({
-  allowedOrigins: ['*.forestadmin.com'],
-  headers: ['Authorization', 'X-Requested-With', 'Content-Type']
+  origin: ['*.forestadmin.com'],
+  allowedHeaders: ['Authorization', 'X-Requested-With', 'Content-Type'],
+  credentials: true
 }));
 
 app.use(jwt({
